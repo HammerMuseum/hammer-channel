a61b1e462737aa44dabeef0e7823fb55a90f37ff
VickiT <vickit@cogapp.com>
2019-12-16 12:01:06 +0000

    Fix JSON string.

--- app/FakeApi.php
+++ app/FakeApi.php
@@ -6,6 +6,7 @@ namespace App;
  * Class FakeApi
  *
  * A fake class used for mocking an API response in dusk tests.
+ * @codingStandardsIgnoreFile
  *
  * @package App
  */
@@ -22,10 +23,12 @@ class FakeApi extends Api
             'success' => true,
             'data' => json_decode($this->getJsonData())
         ];
+
         return $returnData;
     }
 
     /**
+     *
      * @return string
      */
     public function getJsonData()
@@ -33,10 +36,7 @@ class FakeApi extends Api
         return '{
         "asset_id": 100,
   "title": "Mark Bradford\'s Finding Barry",
-  "description": "Since 1999, the museum has commissioned artists to create site-specific projects for the Hammer 
-  Museum Lobby Wall. Mark Bradford created Finding Barry by excavating through 100 layers of paint on the wall. Barry 
-  refers to Barry McGee, one of the earliest Lobby Wall artists. Bradford\'s rendering of a U.S. map shows the 
-  population of people diagnosed with AIDS per 100,000 in each state as of 2009.",
+  "description": "Since 1999, the museum has commissioned artists to create site-specific projects for the Hammer Museum Lobby Wall. Mark Bradford created Finding Barry by excavating through 100 layers of paint on the wall. Barry refers to Barry McGee, one of the earliest Lobby Wall artists. Bradford\'s rendering of a U.S. map shows the population of people diagnosed with AIDS per 100,000 in each state as of 2009.",
   "thumbnail_url": null,
   "video_url": "https://trial10-8.assetbank-server.com/assetbank-trial10/rest/assets/206/content",
   "date_recorded": "2019-12-09 12:05:57",
