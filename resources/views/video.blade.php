2172d8011fe50af9161de7f2f7d0b1cbb7f95ad9
VickiT <vickit@cogapp.com>
2019-12-16 15:36:53 +0000

    Some styling tweaks to the individual video page.

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -9,23 +9,26 @@
 @section('content')
     <div class="video-content">
         @if ($data !== false)
-            <div class="title">
-                <h1>{{ $data['title'] }}</h1>
-            </div>
-
             <div class="video">
-                <video controls type="video/mp4" src="{{ $data['video_url'] }}" width="250">
-
+                <video class="video-js hammer-video" controls type="video/mp4">
+                    <source src="{{ $data['video_url'] }}" />
                 </video>
             </div>
 
-            <div class="date">
-                {{ $data['date_recorded'] }}
+            <div class="hammer-video__information">
+                <div class="title">
+                    <h1>{{ $data['title'] }}</h1>
+                </div>
+
+                <div class="date">
+                    {{ $data['date_recorded'] }}
+                </div>
+
+                <div class="description">
+                    {{ $data['description'] }}
+                </div>
             </div>
 
-            <div class="description">
-                {{ $data['description'] }}
-            </div>
         @else
             <div class="message">
                 <?php echo $message; ?>
