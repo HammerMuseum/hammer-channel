c64cf619fbf303766dfa093d999734a225ae6886
jonw-cogapp <jonw@cogapp.com>
2019-12-17 09:58:11 +0000

    Initial attempt with mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 const mix = require('laravel-mix');
+require('laravel-mix-postcss-config');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +13,25 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-  .postCss('resources/css/app.css', 'public/css');
+mix.webpackConfig({
+  plugins: [
+    new StyleLintPlugin({
+      files: '**/*.css',
+      context: 'resources/css',
+      quiet: true
+    }),
+  ],
+});
+
+// mix.options({
+//   postCss: [
+//     require("stylelint")(),
+//   ]
+// })
+
+mix
+  .js('resources/js/app.js', 'public/js');
+
+mix
+  .postCss('resources/css/app.css', 'public/css')
+  .postCssConfig();
