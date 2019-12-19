4a0ffe85d08776adb5504ec071fec754d7e62281
VickiT <vickit@cogapp.com>
2019-12-19 16:58:47 +0000

    Adding chevron to breadcrumb.

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -9,7 +9,12 @@
 @section('content')
     <div class="video-content">
         <div class="breadcrumb">
-            <a href="/" class="breadcrumb__link">Back to All Videos</a>
+            <a href="/" class="breadcrumb__link">
+                <span class="breadcrumb__icon">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="32"><path d="M.311 30.334L15.34 15.307l1.727 1.727L2.038 32.061.311 30.334zM.095 1.694L1.822-.033l16.236 16.236-1.727 1.727L.095 1.694z"/></svg>
+                </span>
+                <span class="breadcrumb__text">Back to All Videos</span>
+            </a>
         </div>
         @if ($data !== false)
             <div class="video">
