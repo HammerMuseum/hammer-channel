944cbca2d780ed56e7fe9eac88e4b8c20224931b
VickiT <vickit@cogapp.com>
2019-12-19 11:54:07 +0000

    Add breadcrumb link to video template.

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -8,6 +8,9 @@
 
 @section('content')
     <div class="video-content">
+        <div class="breadcrumb">
+            <a href="/" class="breadcrumb__link">Back to All Videos</a>
+        </div>
         @if ($data !== false)
             <div class="video">
                 <video class="video-js hammer-video" controls type="video/mp4">
