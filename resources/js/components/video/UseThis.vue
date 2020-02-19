97157dc3ab4ccbd8d9dd5730181a9a64de02ddd6
VickiT <vickit@cogapp.com>
2020-02-19 10:09:52 +0000

    Use this panel.

---
+++ resources/js/components/video/UseThis.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>Educators can use this video in class for free.</p>
+
+        <p>If you would like to use this video on your website or in broadcast material, please contact us
+            <a href="mailto:hammer.on@hammer.ucla.edu">hammer.on@hammer.ucla.edu</a>
+
+        </p>
+    </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      console.log('Component mounted.');
+    },
+  };
+</script>
