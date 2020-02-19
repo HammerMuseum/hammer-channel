25330757122878dcb643353c95956e5abdb06ceb
VickiT <vickit@cogapp.com>
2020-02-19 11:12:48 +0000

    Update email address in use component.

--- resources/js/components/video/UseThis.vue
+++ resources/js/components/video/UseThis.vue
@@ -2,7 +2,7 @@
     <div>
         <p>Educators can use this video in class for free.</p>
         <p>If you would like to use this video on your website or in broadcast material, please contact us
-            <a href="mailto:hammer.on@hammer.ucla.edu">hammer.on@hammer.ucla.edu</a>
+            <a href="mailto:videos@hammer.ucla.edu">videos@hammer.ucla.edu</a>
         </p>
     </div>
 </template>
