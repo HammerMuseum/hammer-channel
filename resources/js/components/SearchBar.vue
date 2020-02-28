16fba39057d395d11449d1ff2d031e30f757c1a1
VickiT <vickit@cogapp.com>
2020-02-28 16:53:27 +0000

    Some styles for the search overlay.

--- resources/js/components/SearchBar.vue
+++ resources/js/components/SearchBar.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="search-bar-container">
-    <span class="search__submit" @click="showSearch"></span>
-    <div class="search-bar">
-      <input class="search-bar__input" placeholder="Search" type="search" v-model="searchTerm">
-      <router-link class="search__submit" tag="button" :to="{name: 'search', query: {term: this.searchTerm}}">Search</router-link>
+    <div class="search-bar search-bar__hidden">
+      <div class="search-bar__actions">
+        <input class="search-bar__input" placeholder="Search" type="search" v-model="searchTerm">
+        <router-link class="search__submit" tag="button" :to="{name: 'search', query: {term: this.searchTerm}}">Search</router-link>
+      </div>
       <div class="search-bar__options">
         <div class="search-bar__option">
-          or show me everything
+          <span class="tag">or show me everything</span>
+        </div>
+        <div class="search-bar__option">
+          or try <span class="tag">Art</span> <span class="tag">Social justice</span> <span class="tag">Los Angeles</span>
+          <span class="tag">Female directors</span> <span class="tag">Hollywood</span> <span class="tag">Poetry</span>
         </div>
       </div>
     </div>
+    <span class="search__submit" @click="showSearch"></span>
   </div>
 </template>
 
