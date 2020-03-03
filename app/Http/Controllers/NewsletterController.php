39149fd30d1806e06d1ff97b5d12632f14ac76c6
VickiT <vickit@cogapp.com>
2020-03-03 13:59:04 +0000

    Use built in laravel validation.

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -30,32 +30,36 @@ class NewsletterController extends Controller
     /**
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
      */
     public function submit(Request $request)
     {
-        $requestData = $request->get('data');
-        if (!filter_var($requestData['email'], FILTER_VALIDATE_EMAIL)) {
+        try {
+            $this->validate($request, [
+                'data.email' => 'required|email',
+            ]);
+        } catch (\Exception $e) {
             // Email address is invalid
             return response()->json([
                 'success' => false,
                 'message' => 'Please enter a valid email address.'
             ]);
-        } else {
-            // Email address is valid
-            $signupAction = $this->newsletter->subscribe($requestData['email'], [
-                'FNAME' => $requestData['firstname'],
-                'LNAME' => $requestData['lastname']
-            ], 'subscribers');
-            if ($signupAction) {
-                return response()->json([
-                    'success' => true,
-                    'message' => 'Successfully subscribed'
-                ]);
-            }
+        }
+        $requestData = $request->get('data');
+        // Email address is valid
+        $signupAction = $this->newsletter->subscribe($requestData['email'], [
+            'FNAME' => $requestData['firstname'],
+            'LNAME' => $requestData['lastname']
+        ], 'subscribers');
+        if ($signupAction) {
             return response()->json([
-                'success' => false,
-                'message' => 'You are already subscribed to this mailing list.'
+                'success' => true,
+                'message' => 'Successfully subscribed'
             ]);
         }
+        return response()->json([
+            'success' => false,
+            'message' => 'You are already subscribed to this mailing list.'
+        ]);
     }
 }
