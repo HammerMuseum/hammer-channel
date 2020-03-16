2ca64a5c4ab559183d6d3a9fab6710a57c1c3064
VickiT <vickit@cogapp.com>
2020-03-16 16:50:20 +0000

    Fix merge conflict.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,60 +1,32 @@
 // TODO: Is there a way we can use the runtime only build?
-import Vue from 'vue/dist/vue.esm';
+import './bootstrap';
+import Vue from 'vue';
 import VueFilterDateFormat from 'vue-filter-date-format';
-import VueRouter from 'vue-router';
+import router from './router';
 import SocialSharing from 'vue-social-sharing';
-import Home from './components/HomeComponent.vue';
-import Search from './components/SearchComponent.vue';
-import NotFoundComponent from './components/NotFoundComponent.vue';
-import VideoComponent from './components/video/VideoComponent.vue';
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-require('./bootstrap');
-require('videojs-overlay');
-require('video.js');
-require('vue-social-sharing');
+// Move into Video player?
+import 'videojs-overlay';
+import 'video.js';
 
 window.VIDEOJS_NO_BASE_THEME = true;
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+// /**
+//  * The following block of code may be used to automatically register your
+//  * Vue components. It will recursively scan this directory for the Vue
+//  * components and automatically register them with their "basename".
+//  *
+//  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+//  */
 const files = require.context('./', true, /\.vue$/i);
 files.keys().map((key) => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-const routes = [
-  {
-    path: '/', name: 'app', component: Home,
-  },
-  {
-    path: '/search/:params?', name: 'search', component: Search, props: (route) => ({ facetQuery: route.query.facets }),
-  },
-  {
-    path: '/video/:id', name: 'video', component: VideoComponent, props: true,
-  },
-  {
-    path: '*', component: NotFoundComponent,
-  },
-];
-
-const router = new VueRouter({
-  mode: 'history',
-  routes,
-});
-
 Vue.use(VueFilterDateFormat);
-Vue.use(VueRouter);
 Vue.use(SocialSharing);
+
 
 const app = new Vue({ // eslint-disable-line
   el: '#main-content',
   router,
 });
+
