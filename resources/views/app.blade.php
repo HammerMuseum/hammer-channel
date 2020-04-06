5d6dedd08666819e5ccbe604d98fd9482b510d0f
Neil Hawkins <neilh@cogapp.com>
2020-04-06 10:57:27 +0100

    Run linter

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,41 +1,45 @@
 <!DOCTYPE html>
 
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-  <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:url" content="{{ $metadata['url'] }}" />
-    <meta name="twitter:title" content="{{ $metadata['title'] }}" />
-    <meta name="twitter:description" content="{{ $metadata['description'] }}" />
-    <meta name="twitter:image" content="{{ $metadata['image'] }}" />
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta property="og:site_name" content="{{ $metadata['site_name'] }}"/>
-    <meta property="og:url" content="{{ $metadata['url'] }}" />
-    <meta property="og:title" content="{{ $metadata['title'] }}"/>
-    <meta property="og:description" content="{{ $metadata['description'] }}"/>
-    <meta property="og:image" content="{{ $metadata['image'] }}" />
-    <meta property="og:image:type" content="image/jpg" />
-    <meta property="og:image:width" content="320" />
-    <meta property="og:image:height" content="180" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:url" content="{{ $metadata['url'] }}" />
+  <meta name="twitter:title" content="{{ $metadata['title'] }}" />
+  <meta name="twitter:description" content="{{ $metadata['description'] }}" />
+  <meta name="twitter:image" content="{{ $metadata['image'] }}" />
 
-    <link rel="stylesheet" type="text/css" href="/css/main.css" />
-    <title>Hammer Museum | Video Archive</title>
-    <script type="text/javascript">
-        window.INITIAL_STATE = "{!! addslashes(json_encode($state)) !!}";
-    </script>
-  </head>
+  <meta property="og:site_name" content="{{ $metadata['site_name'] }}" />
+  <meta property="og:url" content="{{ $metadata['url'] }}" />
+  <meta property="og:title" content="{{ $metadata['title'] }}" />
+  <meta property="og:description" content="{{ $metadata['description'] }}" />
+  <meta property="og:image" content="{{ $metadata['image'] }}" />
+  <meta property="og:image:type" content="image/jpg" />
+  <meta property="og:image:width" content="320" />
+  <meta property="og:image:height" content="180" />
 
-  <body>
-    <div class="main" id="main-content">
-      <the-header></the-header>
-      <transition name="fade" mode="out-in">
+  <link rel="stylesheet" type="text/css" href="/css/main.css" />
+  <title>Hammer Museum | Video Archive</title>
+  <script type="text/javascript">
+    window.INITIAL_STATE = "{!! addslashes(json_encode($state)) !!}";
+  </script>
+</head>
+
+<body>
+  <div id="app">
+    <the-header></the-header>
+    <transition name="fade" mode="out-in">
+      <main class="main" role="main">
         <router-view></router-view>
-      </transition>
-      <the-footer></the-footer>
-    </div>
-    <script src="{{ (env('APP_ENV') === 'local') ? mix('js/app.js') : '/js/main.js' }}"></script>
-  </body>
+      </main>
+    </transition>
+    <the-footer></the-footer>
+  </div>
+  <script src="{{ (env('APP_ENV') === 'local') ? mix('js/app.js') : '/js/main.js' }}"></script>
+</body>
+
 </html>
