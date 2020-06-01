5b57ec82e47c326209d4c81e49eb40f4290aee3c
Neil Hawkins <neilh@cogapp.com>
2020-06-01 08:46:57 +0100

    Add favicons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,6 +22,14 @@
   <meta property="og:image:width" content="320" />
   <meta property="og:image:height" content="180" />
 
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ee2a7b">
+  <meta name="msapplication-TileColor" content="#ee2a7b">
+  <meta name="theme-color" content="#262626">
+
   <link rel="stylesheet" type="text/css" href="/css/main.css" />
   <link rel="stylesheet" href="https://use.typekit.net/onc8trv.css">
 
