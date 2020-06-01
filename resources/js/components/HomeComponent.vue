8ea5de036222c8f8e1ec1a74dcdb30e1bd89b8d9
Neil Hawkins <neilh@cogapp.com>
2020-06-01 08:21:57 +0100

    Move loader into wrapper component for legibility

--- resources/js/components/HomeComponent.vue
+++ resources/js/components/HomeComponent.vue
@@ -10,56 +10,7 @@
         :active-item="currentSectionInView"
         :classes="['topic-menu']"
       />
-      <ContentLoader
-        v-if="!featured"
-        :width="800"
-        :height="250"
-        :animate="false"
-        primary-color="#c6c6c6"
-        secondary-color="#c6c6c6"
-      >
-        <rect
-          x="425"
-          y="3"
-          rx="2"
-          ry="2"
-          width="361"
-          height="26"
-        />
-        <rect
-          x="425"
-          y="44"
-          rx="2"
-          ry="2"
-          width="361"
-          height="26"
-        />
-        <rect
-          x="6"
-          y="2"
-          rx="2"
-          ry="2"
-          width="400"
-          height="192"
-        />
-        <rect
-          x="425"
-          y="83"
-          rx="2"
-          ry="2"
-          width="361"
-          height="26"
-        />
-        <rect
-          x="425"
-          y="124"
-          rx="2"
-          ry="2"
-          width="361"
-          height="26"
-        />
-      </ContentLoader>
-
+      <Loader v-if="!featured" />
       <Carousel
         v-else
         id="featured"
@@ -131,11 +82,11 @@
 
 <script>
 import axios from 'axios';
-import { ContentLoader } from 'vue-content-loader';
 import { VSkip } from 'vuetensils/src/components';
 import Carousel from './Carousel.vue';
 import CarouselSlide from './CarouselSlide.vue';
 import FeaturedCarouselSlide from './FeaturedCarouselSlide.vue';
+import Loader from './Loader.vue';
 import mixin from '../mixins/getRouteData';
 
 export default {
@@ -143,8 +94,8 @@ export default {
   components: {
     Carousel,
     CarouselSlide,
-    ContentLoader,
     FeaturedCarouselSlide,
+    Loader,
     VSkip,
   },
   filters: {
