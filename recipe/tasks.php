610c73f408a18eefef36e1f47c3f292ef7af61f7
Neil Hawkins <neilh@cogapp.com>
2020-09-04 20:05:51 +0100

    Update permissions settings

--- recipe/tasks.php
+++ recipe/tasks.php
@@ -4,11 +4,8 @@ namespace Deployer;
 
 desc('Change app:permissions');
 task('app:permissions', function () {
-    writeln('change permissions to {{http_user}} for {{release_path}}/public');
-    run('cd {{release_path}} && chown -Rf {{http_user}}:{{http_user}} public/*');
-
-    run('cd {{deploy_path}}/shared && chown -Rf {{app_user}}:{{app_user}} storage');
-    run('cd {{release_path}} && chown -Rf {{app_user}}:{{app_user}} storage bootstrap/cache && chown -Rf {{app_user}}:{{app_user}} public/*');
+    run('cd {{release_path}} && sudo chown -R {{app_user}}:{{app_user}} storage bootstrap/cache');
+    run('cd {{release_path}} && sudo chown -R {{http_user}}:{{http_user}} public');
 });
 
 desc('Execute artisan responsecache:clear');
