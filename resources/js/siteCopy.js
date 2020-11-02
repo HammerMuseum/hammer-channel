03e7ed5f44ebf8f19c1403207dd22e7d0d43a9a7
Neil Hawkins <neilh@cogapp.com>
2020-11-02 15:11:06 +0000

    Update site copy

--- resources/js/siteCopy.js
+++ resources/js/siteCopy.js
@@ -1,8 +1,9 @@
 export default {
   strapline: 'Programming and artist interviews from the Hammer Museum.',
-  aboutHeading: 'Welcome to Hammer Channel',
+  aboutHeading: 'Welcome',
   aboutStrapline: 'This site presents video documentation of the Hammer Museum’s programs and videos related to the museum’s exhibitions, dating back to 2011.',
   emailSignup: 'Stay in the know with emails from the Hammer',
   calendarLink: 'Upcoming programs',
 
 };
+
