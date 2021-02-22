0d901bd24850750ea1873ee08a8a5fbe80424037
Neil Hawkins <neilh@cogapp.com>
2021-02-22 09:49:38 +0000

    Remove accidental deletion

--- resources/js/components/video/VideoPlayer.vue
+++ resources/js/components/video/VideoPlayer.vue
@@ -264,7 +264,7 @@ export default {
         'durationchange',
         'ended',
         'enterFullWindow',
-        'enterpictupicture',
+        'enterpictureinpicture',
         'error',
         'exitFullWindow',
         'fullscreenchange',
