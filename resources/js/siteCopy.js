4c474f66356e9be1acc1bc976f47ed436c90a5f9
iainb-cogapp <75614076+iainb-cogapp@users.noreply.github.com>
2021-02-24 16:27:26 +0000

    Prevent Hammer Museum text from breaking over two lines (#111)

--- resources/js/siteCopy.js
+++ resources/js/siteCopy.js
@@ -1,7 +1,7 @@
 export default {
   strapline: 'Programming and artist interviews from the Hammer Museum.',
   aboutHeading: 'Welcome',
-  aboutStrapline: 'This site presents video documentation of the <a href="https://hammer.ucla.edu">Hammer Museum</a>’s programs and videos related to the museum’s exhibitions, dating back to 2011.',
+  aboutStrapline: 'This site presents video documentation of the <a href="https://hammer.ucla.edu">Hammer&nbsp;Museum</a>’s programs and videos related to the museum’s exhibitions, dating back to 2011.',
   emailSignup: 'Stay in the know with emails from the Hammer',
   calendarLink: 'Upcoming programs',
 };
