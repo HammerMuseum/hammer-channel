aa8fc40fab0bbd181ef82d7458d11d8e966eaea0
Iain Bean <iainb@cogapp.com>
2021-03-19 17:11:34 +0000

    Open Hammer Museum link in about section in new tab

--- resources/js/siteCopy.js
+++ resources/js/siteCopy.js
@@ -1,7 +1,7 @@
 export default {
   strapline: 'Programming and artist interviews from the Hammer Museum.',
   aboutHeading: 'Welcome',
-  aboutStrapline: 'Hammer Channel lets you watch, search, clip, and share videos from the <a href="https://hammer.ucla.edu">Hammer&nbsp;Museum</a>’s programs and exhibitions, from 2005 to the present.',
+  aboutStrapline: 'Hammer Channel lets you watch, search, clip, and share videos from the <a href="https://hammer.ucla.edu" target="_blank">Hammer&nbsp;Museum</a>’s programs and exhibitions, from 2005 to the present.',
   emailSignup: 'Stay in the know with emails from the Hammer',
   calendarLink: 'Upcoming programs',
 };
